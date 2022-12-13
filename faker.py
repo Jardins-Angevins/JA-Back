11da9fa843de38e71222dc000fd75c5a4e4bffd4
Mathieu T <mathieu.toulon@free.fr>
2022-12-13 16:30:12 +0100

    Database handling

---
+++ faker.py
@@ -0,0 +1,17 @@
+from db import Species, Stats, getUUID
+
+
+def fake():
+	temp = Species( 
+		id				= getUUID(),
+		name            = "Coquelicot",
+		scientificName  = "Papaver rhoeas",
+		refImage        = "SomeFakeBase64Data",
+		stats           = Stats(
+			water    = 2,
+			light    = 3,
+			toxicity = 4
+		),
+		images          = ['SomeFakeBase64Data']
+	)
+	temp.save()
