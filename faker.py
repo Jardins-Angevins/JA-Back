ea3e9ae1aee3256587cac4e51ad58f765f39e8df
Mathieu T <mathieu.toulon@free.fr>
2022-12-19 01:22:24 +0100

    Missing a few updates to faker since faker was detach from main

--- faker.py
+++ faker.py
@@ -1,24 +1,26 @@
 from services.db import Species, Stats,UserInput,getUUID
 import datetime
-def fake():
-	Species.create( 
-		nominalNumber	= 992501,
-		name            = "Coquelicot",
-		scientificName  = "Papaver rhoeas",
-		refImage        = "SomeFakeBase64Data",
-		stats           = Stats(
-			water    = 2,
-			light    = 3,
-			toxicity = 4
-		),
-		images          = ['SomeFakeBase64Data','SomeFakeBase64Data']
-	)
-	UserInput.create(
 
-		id=getUUID(),
-		image = "SomeFakeBase64Data",
-		iaGuessedSpeciesId = 992501,
-		latitude = 3,
-		longitude = 3,
-		photoTimestamp = datetime.datetime.now().timestamp()
-	)
+Species.create( 
+	nominalNumber	= 992501,
+	name            = "Coquelicot",
+	scientificName  = "Papaver rhoeas",
+	refImage        = "SomeFakeBase64Data",
+	stats           = Stats(
+		water    = 2,
+		light    = 3,
+		toxicity = 4
+	),
+	images          = ['SomeFakeBase64Data','SomeFakeBase64Data']
+)
+
+UserInput.create(
+
+	id=getUUID(),
+	image = "SomeFakeBase64Data",
+	iaGuessedSpeciesId = 992501,
+	latitude = 3,
+	longitude = 3,
+	photoTimestamp = datetime.datetime.now().timestamp()
+
+)
