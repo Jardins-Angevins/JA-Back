bb48745bc60f174a1e4cc8d407fe578769017b35
Mathieu T <mathieu.toulon@free.fr>
2023-01-02 16:10:16 +0100

    📈 Handling image close to but not exactly the dimension asked

--- tools.py
+++ tools.py
@@ -1,9 +1,22 @@
-import numpy as np
+from PIL import Image
+from io import BytesIO
 import base64
+import numpy as np
 
 from config import config
 
 def base64_to_numpy(image_base64):
 	width,height = config.get('IA.IMAGE_SIZE')
-	image = np.frombuffer(base64.b64decode(bytes(image_base64,"utf-8")), dtype=np.uint8)
-	return np.reshape(image,(width,height,3))
+	# Parse image from b64 png RGB
+	image = np.array( Image.open(BytesIO(base64.b64decode(image_base64))) )
+	# Check if image is at least near to the correct dimensions
+	w,h,c = image.shape
+	if abs(width - w) > 5 or abs(height - h) > 5 or (c != 3 and c != 4):
+		raise ValueError()
+	# If it is not exactly what we ask add black border
+	if w != width or h != height:
+		result = np.zeros( (width,height,3) )
+		result[ :min(w,width) , :min(h,height) , 0:3 ] = image[ :min(w,width) , :min(h,height) , 0:3 ]
+		return result
+	else:
+		return image
